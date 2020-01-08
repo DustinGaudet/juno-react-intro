dae87adc455521d26699cd77a30ec54830dbf538
DustinGaudet <dustin@dustingaudet.com>
2020-01-08 08:33:57 -0500

    replace author tags with h3 tags

--- src/Book.js
+++ src/Book.js
@@ -3,9 +3,7 @@ import React from "react";
 const Book = () => (
   <section className="book">
     <h2>My Book Title</h2>
-    <author>
-      <strong>Book Author</strong>
-    </author>
+    <h3>Book Author</h3>
     <p>
       <em>A short description about the book can go here!</em>
     </p>
