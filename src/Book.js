30cb58d004fed355f28eb3b55eb44dfb6d4d4c46
DustinGaudet <dustin@dustingaudet.com>
2020-01-07 20:40:38 -0500

    Create a stateless Book component

---
+++ src/Book.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Book = () => (
+  <section className="book">
+    <h2>My Book Title</h2>
+    <h3>Book Author</h3>
+    <p>
+      <em>A short description about the book can go here!</em>
+    </p>
+    <button>Delete Book</button>
+  </section>
+);
+
+export default Book;
