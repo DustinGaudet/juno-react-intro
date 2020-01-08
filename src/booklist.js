28748d8456aee4a8ad344bc12d92151d82842db6
DustinGaudet <dustin@dustingaudet.com>
2020-01-07 21:55:51 -0500

    Add a mock book list

---
+++ src/booklist.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    description: "Elizabeth Bennet learns the repercussions of hasty judgments."
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    description: "The consequences of government overreach and totalitarianism."
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    description:
+      "An exploration of decadence and idealism in the Roaring Twenties."
+  }
+];
